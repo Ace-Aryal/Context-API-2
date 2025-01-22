d0c5884bff7372ed4a59f47c0276bd6743ec54ef
Dipesh Aryal <153974691+Ace-Aryal@users.noreply.github.com>
2025-01-22 18:54:25 +0545

    Update ThemeBtn.jsx
    Added comments

--- my-project/src/Components/ThemeBtn.jsx
+++ my-project/src/Components/ThemeBtn.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import useTheme from "../context/Theme"
 function ThemeBtn() {
-const {themeMode , lighttheme , darktheme} = useTheme()
+const {themeMode , lighttheme , darktheme} = useTheme() // the btn is contained inside provider
+    // so we can access the value of the context (global in our case) inside any component
+    // via our custom useTheme() hook , which is equivalent to importing
+    // the context and using useContext(ThemContext) here , so we are basically 
+    // making our code modular
     
 
 function handleClick (e){
@@ -22,7 +26,7 @@ function handleClick (e){
                   value=""
                   className="sr-only peer"
                   onChange={handleClick}
-                  checked= {themeMode === "dark"}
+                  checked= {themeMode === "dark"} // if current themeMode (global variable : in our case) is "dark" then checked otherwise unchecked 
               />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
               <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
